871250404ce3c98109cb2744d4edcfbe65da559f
mtarasenko <wick71@yandex.ru>
2022-10-18 15:11:24 +0300

    Edit model Picture

--- image_app/models.py
+++ image_app/models.py
@@ -4,11 +4,12 @@ from django.db import models
 class Picture(models.Model):
     """ Изображение """
     name = models.CharField(max_length=100, verbose_name='Название')
-    url = models.URLField(null=True, blank=True)
-    picture = models.ImageField(upload_to='site_media/')
-    width = models.IntegerField(verbose_name='Ширина')
-    height = models.IntegerField(verbose_name='Высота')
-    parent_picture = models.IntegerField(null=True, blank=True, verbose_name='ID Родительского изображения')
+    url = models.URLField(null=True, blank=True, verbose_name='Ссылка на изображение')
+    picture = models.ImageField(upload_to='site_media/', blank=True, null=True, verbose_name='Изображение')
+    width = models.PositiveIntegerField(verbose_name='Ширина')
+    height = models.PositiveIntegerField(verbose_name='Высота')
+    parent_picture = models.ForeignKey('Picture', null=True, blank=True, on_delete=models.CASCADE,
+                                       verbose_name='Родительское изображение', related_name='parent')
 
     def __str__(self):
         return self.name
--- settings/settings.py
+++ settings/settings.py
@@ -18,8 +18,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party
     'rest_framework',
 
+    # my apps
     'image_app',
 ]
 
